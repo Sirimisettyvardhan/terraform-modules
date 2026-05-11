8284e67c2e53aa9f46a1f39b1e7275ffac5d3544
VSIRIMIX <109593537+Sirimisettyvardhan@users.noreply.github.com>
2026-05-11 17:09:06 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,20 @@
+variable "region" {
+description="name of a region"
+type=string 
+}
 
+variable "cidr_block" {
+type=string
+}
+
+variable "ami" {
+type=string
+}
+
+varaible "instance_type" {
+type=string
+}
+
+variable "bucket_name" {
+type=string
+}
